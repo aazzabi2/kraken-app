35c8889fdce94c78ca9a8f7530d2887f649cafdb
Arafet <arafet.azzabi@esprit.tn>
2025-02-12 19:01:03 +0100

    add routes

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { DataUploadComponent } from './components/data-upload/data-upload.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: DataUploadComponent,
+  },
+];
